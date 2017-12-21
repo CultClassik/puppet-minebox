937b573647c59a8e6efe86e82945c4d99ec45ab8
Chris Diehl <cultclassik@gmail.com>
2017-12-20 20:35:03 -0600

    Testing

--- manifests/nvidia/install.pp
+++ manifests/nvidia/install.pp
@@ -8,15 +8,20 @@
 #   include minebox::nvidia::install
 class minebox::nvidia::install {
 
+  require stdlib
+
+  $nvidia_packages = concat($minebox::packages_xorg, $minebox::nvidia_driver)
+
   ensure_packages(
-    $minebox::packages_xorg,
+    #$minebox::packages_xorg,
+    $nvidia_packages,
     {
       ensure => present,
     }
   )
 
-  -> package { $minebox::nvidia_driver :
-    ensure => present,
-  }
+  #package { $minebox::nvidia_driver :
+  #  ensure => present,
+  #}
 
 }
