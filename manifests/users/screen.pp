7e39b2f53fbd1f9e8ce2d3ab8822c54918b62c83
Chris Diehl <cultclassik@gmail.com>
2017-12-20 23:13:10 -0600

    Updated screenrc template

--- manifests/users/screen.pp
+++ manifests/users/screen.pp
@@ -21,7 +21,6 @@ class minebox::users::screen {
       owner     => $minebox::miner_user,
       group     => $minebox::miner_group,
       mode      => '0774',
-      #subscribe => File["/home/${minebox::miner_user}"],
     }
   }
 
