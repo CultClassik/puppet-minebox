7df889b44b920a4ed7ebacfdae2566b27ddff7d8
Chris Diehl <cultclassik@gmail.com>
2017-12-22 11:53:15 -0600

    Added auto mine in screenrc for amd miners

--- manifests/users/screen.pp
+++ manifests/users/screen.pp
@@ -9,19 +9,32 @@
 #   include minebox::users::screen
 class minebox::users::screen {
 
+
   if $::minebox::gpu_type == 'nvidia' {
     file { '/home/miner/.screenrc' :
       ensure  => file,
       content => epp(
         'minebox/nvidia/screenrc.epp',
         {
-          'gpu_cfg' => $minebox::nv_gpus
+          'gpu_cfg' => $minebox::nv_gpus,
           }
         ),
       owner   => $minebox::miner_user,
       group   => $minebox::miner_group,
       mode    => '0774',
     }
+  } else {
+    file { '/home/miner/.screenrc' :
+      ensure  => file,
+      content => epp(
+        'minebox/amd/screenrc.epp',
+        {
+          'mining_script' => "home/${minebox::mining_user}/claymore",
+          }
+        ),
+      owner   => $minebox::miner_user,
+      group   => $minebox::miner_group,
+      mode    => '0774',
   }
 
   ##### ADD ELSE HERE TO GENERATE SCREENRC FOR AMD SYSTEMS
