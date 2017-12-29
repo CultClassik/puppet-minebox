b3784123d48e567df1136d20457769af07c2e2ca
Chris Diehl <cultclassik@gmail.com>
2017-12-29 07:38:29 -0600

    Added ewbf miner type

---
+++ manifests/docker/containers/equihas_ewbf_nv.pp
@@ -0,0 +1,38 @@
+# minebox::docker::containers::equihash_ewbf_nv
+#
+# Creates a NVidia Docker GPU container instance running ethminer
+#
+# @summary Creates a NVidia Docker GPU container instance running ethminer
+#
+# @example
+#   minebox::docker::containers::equihash_ewbf_nv { 'namevar': }
+define minebox::docker::containers::equihash_ewbf_nv(
+  Hash $gpu,
+  String $docker_image = 'cultclassik/equihash-ewbf-nv',
+  String $image_tag = 'latest',
+)
+{
+
+  $worker = "${trusted['hostname']}-${gpu['id']}"
+
+  docker::run { "m-nv${gpu['id']}" :
+    ensure                   => present,
+    image                    => "${docker_image}:${image_tag}",
+    hostname                 => "${facts['hostname']}-${gpu['id']}",
+    env                      => [
+      "WORKER=${worker}",
+      "T_ADDR=${minebox::accounts['zcl']}",
+      'NVIDIA_DRIVER_CAPABILITIES=compute,utility',
+      "NVIDIA_VISIBLE_DEVICES=${gpu['id']}",
+    ],
+    dns                      => ['8.8.8.8', '8.8.4.4'],
+    expose                   => ['42000'],
+    extra_parameters         => [
+      '--runtime=nvidia',
+    ],
+    remove_container_on_stop => true,
+    remove_volume_on_stop    => true,
+    pull_on_start            => true,
+  }
+
+}
---
+++ manifests/docker/types/equihash_ewbf_nv.pp
@@ -0,0 +1,11 @@
+# minebox::docker::types::equihash_ewbf_nv
+#
+# A description of what this defined type does
+#
+# @summary A short summary of the purpose of this defined type.
+#
+# @example
+#   minebox::docker::types::equihash_ewbf_nv { 'namevar': }
+define minebox::docker::types::equihash_ewbf_nv(
+) {
+}
