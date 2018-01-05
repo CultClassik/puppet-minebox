e6061a63d6ded12f2a99f83e39c3838af3df2d0b
Chris Diehl <cultclassik@gmail.com>
2018-01-05 07:00:41 -0600

    Updated xmr cpu mining docker image

--- manifests/docker/containers/xmr_cpu.pp
+++ manifests/docker/containers/xmr_cpu.pp
@@ -13,27 +13,33 @@ class minebox::docker::containers::xmr_cpu(
   $cpu_shares = '800',
 )
 {
-  require docker
-
   docker::run { 'xmr-cpu-miner' :
-    ensure                   => present,
-    image                    => "${docker_image}:${image_tag}",
-    dns                      => ['8.8.8.8', '8.8.4.4'],
-    env                      => [
-      "username=${minebox::accounts['xmr']}",
-    ],
-    extra_parameters         => [
-      '--restart=always',
-      "--cpu-shares=${cpu_shares}",
-    ],
-    volumes                  => [
-      '/etc/localtime:/etc/localtime',
-      '/dev/hugepages:/sys/kernel/mm/hugepages',
+    ensure => present,
+    image  => "${docker_image}:${image_tag}",
+  }
+
+  $facts['processors']['models'].each |Integer $index, Straing $value| {
+
+    docker::run { "m-cpu-${index}" :
+      ensure                   => present,
+      image                    => "${docker_image}:${image_tag}",
+      dns                      => ['8.8.8.8', '8.8.4.4'],
+      env                      => [
+        "username=${minebox::accounts['xmr']}",
       ],
-    remove_container_on_stop => true,
-    remove_volume_on_stop    => true,
-    pull_on_start            => true,
-    subscribe                => Exec['Refresh mounts for Huge Pages'],
+      extra_parameters         => [
+        '--restart=always',
+        "--cpu-shares=${cpu_shares}",
+      ],
+      volumes                  => [
+        '/etc/localtime:/etc/localtime',
+        '/dev/hugepages:/sys/kernel/mm/hugepages',
+        ],
+      remove_container_on_stop => true,
+      remove_volume_on_stop    => true,
+      pull_on_start            => true,
+      subscribe                => Exec['Refresh mounts for Huge Pages'],
+    }
   }
 
 }
