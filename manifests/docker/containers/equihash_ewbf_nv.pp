d894a86c0bdbabb24455f1964217924171914e73
Chris Diehl <cultclassik@gmail.com>
2018-01-06 16:32:38 -0600

    Changed default pool names due to issues with us.miningspeed.com

--- manifests/docker/containers/equihash_ewbf_nv.pp
+++ manifests/docker/containers/equihash_ewbf_nv.pp
@@ -10,9 +10,9 @@ define minebox::docker::containers::equihash_ewbf_nv(
   Hash $gpu,
   String $docker_image = 'cultclassik/equihash-ewbf-nv',
   String $image_tag = 'latest',
-  String $pool_name = 'us.miningspeed.com',
+  String $pool_name = 'mining.miningspeed.com',
   String $pool_port = '3053',
-  String $pool_name_1 = 'eu.miningspeed.com',
+  String $pool_name_1 = 'us.miningspeed.com',
   String $pool_port_1 = '3053',
 )
 {
