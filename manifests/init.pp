d94165e831d38bc64c03452290ad9914507c7aef
Chris Diehl <cultclassik@gmail.com>
2018-01-13 07:20:50 -0600

    Move more params to hiera

--- manifests/init.pp
+++ manifests/init.pp
@@ -15,47 +15,15 @@ class minebox(
   Array $miner_user_groups,
   String $miner_user_pwd,
   String $miner_user_ssh_key,
+  Hash $accounts,
   Hash $nv_conf,
   Hash $amd_conf,
   Boolean $cpu_mining,
   Array $packages_base,
   Array $packages_xorg,
   Array $folders,
-  Hash $miners = {
-    'claymore' => {
-      'version' => '10.3',
-      'source'  => 'https://s3-us-west-1.amazonaws.com/mastermine/minebox/claymore_Ethereum%2BDecred_Siacoin_Lbry_Pascal_gpu_v10.3_+LINUX.tar.gz',
-      'file'    => 'claymore.tar.gz',
-      'creates' => 'ethdcrminer64',
-      },
-    'ethminer' => {
-      'version' => '0.12.0',
-      'source'  => 'https://github.com/ethereum-mining/ethminer/releases/download/v0.12.0/ethminer-0.12.0-Linux.tar.gz',
-      'file'    => 'ethminer.tar.gz',
-      'creates' => 'ethminer',
-      },
-    'claymore-equi' => {
-      'version' => '12.6',
-      'source'  => 'https://s3-us-west-1.amazonaws.com/mastermine/minebox/claymore_ZCash_AMD_GPU_Miner_v12.6_LINUX.tar.gz',
-      'file'    => 'claymore-equi.tar.gz',
-      'creates' => 'zecminer64',
-    },
-    'dstm-equihash' => {
-      'version' => '0.5.8',
-      'source'  => 'https://s3-us-west-1.amazonaws.com/mastermine/minebox/zm_0.5.8.tar.gz',
-      'file'    => 'zm_0.5.8.tar.gz',
-      'creates' => 'zm'
-    }
-  },
-  Hash $tools = { },
-  Hash $accounts = {
-    'eth' => '0x96ae82e89ff22b3eff481e2499948c562354cb23',
-    'lbc' => 'cultclassik',
-    'xmr' => '447vxA7StEu5Ht9p8MiWNmhLo48dYnfwPGUYtxUAArxKD6DkSthnQiVL843NKEC1oGTS6Gmu3XaoK3uBcQ118zXaFPjLdxz',
-    'sia' => '9e4337a945bdcbb7e9edfc6889a89202ea4e72d1ea389d8090bf117656e83bcb223626f10681',
-    'zcl' => 't1R1VkvxFBFsfdU2MaRnJSHvnkVNhuFiydF',
-    'zec' => 'your_t_addr_here',
-  },
+  Hash $miners,
+  Hash $tools = {},
   #move this to the miners hash param
   Hash $claymore = {
     'dcri'     => 8,
