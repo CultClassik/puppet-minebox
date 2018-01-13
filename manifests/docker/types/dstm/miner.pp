51dccb4619a0de9b505517ebe8d880bce4bac4af
Chris Diehl <cultclassik@gmail.com>
2018-01-13 13:19:25 -0600

    Almost finished with dstm defined type

--- manifests/docker/types/dstm/miner.pp
+++ manifests/docker/types/dstm/miner.pp
@@ -3,9 +3,35 @@
 # A description of what this defined type does
 #
 # @summary A short summary of the purpose of this defined type.
+# NOTE! The command needs to be built in the calling class.
 #
 # @example
 #   minebox::docker::types::dstm::miner { 'namevar': }
 define minebox::docker::types::dstm::miner(
+  Hash $gpu,
+  String $command = '--server us.miningspeed.com --port 3052 --user  t1R1VkvxFBFsfdU2MaRnJSHvnkVNhuFiydF.miner043 --pass x --telemetry 0.0.0.0:2222',
+  String $repo = 'cryptojunkies',
+  String $image = 'dstm',
+  String $tag = 'latest',
 ) {
+
+ # I think we can make this a generic type of equihash - doesn't matter what miner we're using just pass
+ # in the correct cmd to run?
+  $container_name = "gpu-nv${gpu['id']}"
+
+  docker::run { $container_name :
+    ensure                   => present,
+    image                    => "${repo}/${image}:${tag}",
+    hostname                 => $container_name,
+    env                      => [ "NVIDIA_VISIBLE_DEVICES=${gpu['id']}" ],
+    volumes                  => [ '/etc/localtime:/etc/localtime' ],
+    dns                      => [ '8.8.8.8', '8.8.4.4 '],
+    expose                   => [ '2222' ],
+    extra_parameters         => [ '--runtime=nvidia' ],
+    command                  => $command,
+    remove_container_on_stop => true,
+    remove_volume_on_stop    => true,
+    pull_on_start            => true,
+  }
+
 }
