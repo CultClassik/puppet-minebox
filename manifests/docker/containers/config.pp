7e7e90ad87f58652eb50aa3eb5fa7dc4cfb4f6a6
Chris Diehl <cultclassik@gmail.com>
2018-01-14 10:08:59 -0600

    Testing

--- manifests/docker/containers/config.pp
+++ manifests/docker/containers/config.pp
@@ -18,7 +18,7 @@ class minebox::docker::containers::config (
   $gpus.each |Hash $gpu| {
 
     #$docker_image = "$gpu['miner']['repo']/$gpu['miner']['image']:$gpu['miner']['tag']"
-    docker_image = $gpu['miner']['tag']
+    $docker_image = $gpu['miner']['tag']
 
     #$docker_image = "${gpu['miner']['repo']}/${gpu['miner']['image']}"
     #$docker_image = $miner[repo]
