f057916abc0f7c555a530d12a528d2ae5eb46122
Chris Diehl <cultclassik@gmail.com>
2018-01-14 10:33:12 -0600

    Complete definde type updates

--- manifests/docker/types/dstm/miner.pp
+++ manifests/docker/types/dstm/miner.pp
@@ -13,18 +13,9 @@ define minebox::docker::types::dstm::miner(
   String $image,
   String $command,
 ) {
-  # String $repo = 'cryptojunkies',
-  # String $image = 'dstm',
-  # String $tag = 'latest',
-
- # I think we can make this a generic type of equihash - doesn't matter what miner we're using just pass
- # in the correct cmd to run?
- # Make sure that hostname interpolates correctly.
-  #$container_name = "gpu-nv${gpu['id']}"
 
   docker::run { $container_name :
     ensure                   => present,
-    #image                    => "${repo}/${image}:${tag}",
     image                    => $image,
     hostname                 => "${::hostname}-gpu${gpu_id}",
     env                      => [ "NVIDIA_VISIBLE_DEVICES=${gpu_id}" ],
