16af0e1b6dd217b421bc55920a94f76aa1469586
Chris Diehl <cultclassik@gmail.com>
2018-01-14 06:59:39 -0600

    Test merge for gpu config

--- manifests/docker/containers/config.pp
+++ manifests/docker/containers/config.pp
@@ -13,8 +13,10 @@ class minebox::docker::containers::config (
   notify { 'Applying Docker GPU Miner Container Class..' : }
 
   ###testing merge
-  #$t_defaults = lookup('$minebox::miner_defaults::nv::eth')
-  $gpu_conf = deep_merge($minebox::miner_defaults::nv::eth, $minebox::nv_conf::gpus['0'])
+  $t_defaults = lookup('$minebox::miner_defaults::nv::eth')
+  $t_gpu = lookup('$minebox::nv_conf::gpus.0')
+  $gpu_conf = deep_merge($t_defaults, $t_gpu)
+  #$gpu_conf = deep_merge($minebox::miner_defaults::nv::eth, $minebox::nv_conf::gpus['0'])
   $worker = $minebox::nv_conf::gpus['0']
   $command = regsubst($gpu_conf['command'], 'WORKER_ID', $worker)
   notify { "${command}" : }
