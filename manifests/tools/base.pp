75bff85aa9bdae6b92aa3299e6695d369ad3805a
Chris Diehl <cultclassik@gmail.com>
2018-01-20 06:35:01 -0600

    Testing

--- manifests/tools/base.pp
+++ manifests/tools/base.pp
@@ -16,11 +16,11 @@ class minebox::tools::base {
     $miners = lookup('minebox::miners.hybrid')
   }
 
-  if lookup('minebox::tools.hybrid') == undef {
-    $tools = {}
-  } else {
+  #if lookup('minebox::tools.hybrid') == undef {
+  #  $tools = {}
+  #} else {
     $tools = lookup('minebox::tools.hybrid')
-  }
+  #}
 
   if $minebox::nv_conf::enable == true {
     $miners = deep_merge($miners, lookup('minebox::miners.nv'))
@@ -41,6 +41,7 @@ class minebox::tools::base {
     miners     => $miners,
   }
 
+  # NOTE!  This won't do anything, possibly
   class { 'minebox::tools::tools' :
     files_path => $tools_path,
     tools      => $tools,
