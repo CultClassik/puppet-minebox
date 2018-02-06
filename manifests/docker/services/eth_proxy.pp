27ae51cd8c544290e7b41690e34f1e88e75bdd2f
Chris Diehl <cultclassik@gmail.com>
2018-02-06 06:19:35 -0600

    Testing

--- manifests/docker/services/eth_proxy.pp
+++ manifests/docker/services/eth_proxy.pp
@@ -6,6 +6,43 @@
 #
 # @example
 #   include minebox::docker::services::eth_proxy
-class minebox::docker::services::eth_proxy{
+class minebox::docker::services::eth_proxy(
+  Boolean $enable,
+  Boolean $debug,
+  String $wallet,
+  String $pool_host,
+  String $pool_port,
+  String $pool_host_failover1,
+  String $pool_port_failover1,
+  String $pool_host_failover2,
+  String $pool_port_failover2,
+  String $pool_host_failover3,
+  String $pool_port_failover3,
+  String $service_name,
+  String $image_name,
+  String $traefik_host_name,
+  String $traefik_port,
+  String $docker_network_web,
+  Integer $swarm_replicas
+  # Network to be dedicated to miner traffic:
+  #String $docker_network_stratum,
+) {
+
+  if $enable == true {
+
+    docker::services { $service_name :
+      create       => true,
+      service_name => $service_name,
+      image        => $image_name,
+      replicas     => $swarm_replicas,
+      extra_params => [
+        "--network ${docker_network_web}",
+        '--label traefik.enable=true',
+        "--label traefik.port=${traefik_port}",
+        "--label traefik.frontend.rule=Host:${traefik_host_name}",
+        # Make constraint optional:
+        #'--constraint=node.role==manager',
+      ],
+    }
 
 }
