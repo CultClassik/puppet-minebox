2424020338e50297218b91315eef1d18f358760e
Chris Diehl <cultclassik@gmail.com>
2018-02-05 21:33:51 -0600

    Update docker config class

--- manifests/docker/config.pp
+++ manifests/docker/config.pp
@@ -4,6 +4,9 @@
 #
 # @summary A short summary of the purpose of this class
 #
+#  Add this class to your swarm manager and set hiera values to make use of it.
+#  This class needs a lot more work to be super useful.
+#
 # @example
 #   include minebox::docker::config
 class minebox::docker::config(
